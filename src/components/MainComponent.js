5bd42ae6ef9bde4a8866ea1450ac406696dfe394
DE160490 <thanhncde160490@fpt.edu.vn>
2023-07-19 07:51:05 +0700

    Fetch Handling Errors

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -122,13 +122,13 @@ class Main extends Component {
         <DishDetail
           dish={
             this.props.dishes.dishes.filter(
-              (dish) => dish.id === parseInt(match.params.dishId, 10)
+              (dish) => dish.id === parseInt(dishId, 10)
             )[0]
           }
           isLoading={this.props.dishes.isLoading}
           errMess={this.props.dishes.errMess}
           comments={this.props.comments.comments.filter(
-            (comment) => comment.dishId === parseInt(match.params.dishId, 10)
+            (comment) => comment.dishId === parseInt(dishId, 10)
           )}
           commentsErrMess={this.props.comments.errMess}
           addComment={this.props.addComment}
